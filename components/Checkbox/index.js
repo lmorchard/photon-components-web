52fdbb5485d3a2215f84ae0391c60ae97e4407aa
Les Orchard <me@lmorchard.com>
2018-11-08 13:47:05 -0500

    Drop checkbox label hacks in favor of -moz-appearance
    Fixes #59

--- components/Checkbox/index.js
+++ components/Checkbox/index.js
@@ -4,11 +4,13 @@ import classNames from "classnames";
 import "./index.css";
 
 export const Checkbox = ({ disabled, label, ...props }) => {
-  return (
+  return label ? (
     <label className={classNames("checkbox", { "checkbox--disabled": disabled })}>
       <input {...props} disabled={disabled} className="checkbox__field" type="checkbox" />
       <span className="checkbox__label">{label}</span>
     </label>
+  ) : (
+    <input {...props} disabled={disabled} type="checkbox" />
   );
 };
 
